b22e95addfe98f1e31a3f2207613f78ebab46c5f
shamratPG <shamratpg@gmail.com>
2022-10-11 13:58:31 +0600

    created NavBar and defined some routes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand>
+                    <Link className='nav-link' to='/'>Web Dev Quiz</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+
+                    <Nav className='w-100 d-flex justify-content-end'>
+                        <Nav.Link>
+
+                            <Link className='nav-link' to='/home'>Home</Link>
+                        </Nav.Link>
+                        <Nav.Link>
+
+                            <Link className='nav-link' to='/blogs'>Blogs</Link>
+                        </Nav.Link>
+                        <Nav.Link>
+
+                            <Link className='nav-link' to='/stats'>Stats</Link>
+                        </Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stats = () => {
+    return (
+        <div>
+            <h3>This is stats</h3>
+        </div>
+    );
+};
+
+export default Stats;
