85e39de5bc1a6606d38f1e31f0498aef8c74fa89
shamratPG <shamratpg@gmail.com>
2022-10-12 12:28:40 +0600

    added tostify

--- src/components/AllQuiz/AllQuiz.js
+++ src/components/AllQuiz/AllQuiz.js
@@ -4,13 +4,13 @@ import Quiz from '../Quiz/Quiz';
 
 const AllQuiz = () => {
     const allQuiz = useLoaderData();
-    const { questions } = allQuiz.data
+    const { questions } = allQuiz.data;
     // console.log(allQuiz.data.name);
     return (
         <div className='my-5'>
             <h2 className='my-4'>Quiz for {allQuiz.data.name}</h2>
             {
-                questions.map((q, i) => <Quiz key={q.id} quiz={q} index={i}></Quiz>)
+                questions.map((q, i) => <Quiz key={q.id} quiz={q} index={i} ></Quiz>)
             }
         </div>
     );
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import Option from '../Option/Option';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Quiz = ({ quiz, index }) => {
-    console.log(quiz);
+    // console.log(quiz);
     const { question, options, correctAnswer } = quiz;
+    const correctAns = () => toast.success("Correct Answer", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+        theme: "dark",
+        hideProgressBar: true,
+        draggable: false,
+        pauseOnHover: false,
+    });
+    const incorrectAns = () => toast.error("Incorrect Answer", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+        theme: "dark",
+        hideProgressBar: true,
+        draggable: false,
+        pauseOnHover: false,
+    });
     // console.log(quiz);
     const checkAns = ans => {
-        const result = ans === correctAnswer ? 'Correct' : 'not Correct';
-        console.log(result)
+        if (ans === correctAnswer) {
+            correctAns()
+        } else incorrectAns();
     }
     return (
 
         <div className='w-75 m-auto my-4 shadow p-3 px-4 text-start'>
             <div>
-                <h5><span className='text-decoration-underline'> Question No. {index + 1}: </span> {question}</h5>
+                <h5 className='text-center text-secondary'><span className='text-decoration-underline'> Question No. {index + 1}: </span> {question}</h5>
                 <hr />
             </div>
             <div className="row">
                 {
                     options.map((option, i) => <Option key={i} option={option} question={question} checkAns={checkAns}></Option>)
                 }
+                <ToastContainer />
             </div>
         </div>
 
